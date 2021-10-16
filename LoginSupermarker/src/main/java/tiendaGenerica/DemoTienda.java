f85d27e71f56b4574145e9e3db86b102ac9e32e4
CAROLINA MARÍA CAMACHO ZAMBRANO <36022220+ccamacho5237@users.noreply.github.com>
2021-10-15 22:16:56 -0500

    Add files via upload

--- LoginSupermarker/src/main/java/tiendaGenerica/DemoTienda.java
+++ LoginSupermarker/src/main/java/tiendaGenerica/DemoTienda.java
@@ -1,1 +1,52 @@
+package tiendaGenerica;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoTienda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoTienda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String nombre = request.getParameter("usuario");
+		String contraseña = request.getParameter("contraseña");
+		PrintWriter writer = response.getWriter();
+		if( nombre != null && contraseña != null) {
+			writer.println("Bienvenido "+nombre+" a mi Tienda Groumet");
+		}
+		else
+			writer.println("Error: Nombre o Contraseña faltante!");
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
